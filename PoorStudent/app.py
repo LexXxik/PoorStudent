aca133f0d6a8f38b3a9754323b2c157d5a8057f4
Adrian Bartko <2482586B@student.gla.ac.uk>
2021-09-09 12:43:48 +0100

    Adding beautifulsoup

--- PoorStudent/app.py
+++ PoorStudent/app.py
@@ -4,6 +4,8 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
