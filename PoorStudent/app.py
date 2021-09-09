1d7712bc4bfb0c0fff3ae25cc56065c760845606
Adrian Bartko <2482586B@student.gla.ac.uk>
2021-09-09 15:40:16 +0100

    Work on front end.

--- PoorStudent/app.py
+++ PoorStudent/app.py
@@ -3,7 +3,7 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -15,7 +15,7 @@ wsgi_app = app.wsgi_app
 @app.route('/')
 def hello():
     """Renders a sample page."""
-    return "Hello World!"
+    return render_template("shopping.html")
 
 if __name__ == '__main__':
     import os
